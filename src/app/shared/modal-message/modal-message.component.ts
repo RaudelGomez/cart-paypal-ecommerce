56d001787156c099f41b7ea0ff158dcc7cd236bf
Raudel Gómez Smith <gomezsmith.raudel@gmail.com>
2024-08-31 00:26:08 +0200

    creating all components

---
+++ src/app/shared/modal-message/modal-message.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-modal-message',
+  standalone: true,
+  imports: [],
+  templateUrl: './modal-message.component.html',
+  styleUrl: './modal-message.component.scss'
+})
+export class ModalMessageComponent {
+
+}
