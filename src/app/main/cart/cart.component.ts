6f1db643bef116cba500a9ee4c3ffe3c4bc8da61
Raudel Gómez Smith <gomezsmith.raudel@gmail.com>
2024-09-06 18:13:28 +0200

    app/hardcore

--- src/app/main/cart/cart.component.ts
+++ src/app/main/cart/cart.component.ts
@@ -1,12 +1,14 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { CartItemComponent } from './cart-item/cart-item.component';
 
 @Component({
   selector: 'app-cart',
   standalone: true,
-  imports: [],
+  imports: [CartItemComponent, CommonModule],
   templateUrl: './cart.component.html',
   styleUrl: './cart.component.scss'
 })
 export class CartComponent {
-
+  cardItems = [1,2,3]
 }
