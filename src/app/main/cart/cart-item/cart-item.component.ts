541ffe4f565e9732aa9366d252a00aeed2b367b0
Raudel Gómez Smith <gomezsmith.raudel@gmail.com>
2024-09-06 18:58:18 +0200

    cart/dummy data hardcore

--- src/app/main/cart/cart-item/cart-item.component.ts
+++ src/app/main/cart/cart-item/cart-item.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import {MatIconModule} from '@angular/material/icon';
 
 @Component({
   selector: 'app-cart-item',
   standalone: true,
-  imports: [],
+  imports: [MatIconModule],
   templateUrl: './cart-item.component.html',
   styleUrl: './cart-item.component.scss'
 })
--- src/app/main/product-list/product-item/product-item.component.ts
+++ src/app/main/product-list/product-item/product-item.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
+import {MatIconModule} from '@angular/material/icon';
 
 @Component({
   selector: 'app-product-item',
   standalone: true,
-  imports: [MatButtonModule],
+  imports: [MatButtonModule, MatIconModule],
   templateUrl: './product-item.component.html',
   styleUrl: './product-item.component.scss'
 })
