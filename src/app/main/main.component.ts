6402adacf00b996245b80dd4cfd158252467cdb1
Raudel Gómez Smith <gomezsmith.raudel@gmail.com>
2024-09-06 15:13:45 +0200

    main-componet/fixing UI

--- src/app/main/main.component.ts
+++ src/app/main/main.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { FooterComponent } from '../shared/footer/footer.component';
+import { HeaderComponent } from '../shared/header/header.component';
 
 @Component({
   selector: 'app-main',
   standalone: true,
-  imports: [FooterComponent],
+  imports: [FooterComponent, HeaderComponent],
   templateUrl: './main.component.html',
   styleUrl: './main.component.scss'
 })
