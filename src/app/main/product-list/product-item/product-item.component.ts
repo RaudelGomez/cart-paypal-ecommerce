bce1eccf8b925c86b4aa758d7a5e963bb65cd01b
Raudel Gómez Smith <gomezsmith.raudel@gmail.com>
2024-09-09 10:35:44 +0200

    productlist/description fixed

--- src/app/main/product-list/product-item/product-item.component.ts
+++ src/app/main/product-list/product-item/product-item.component.ts
@@ -2,15 +2,35 @@ import { Component, Input } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
 import {MatIconModule} from '@angular/material/icon';
 import { ProductClass } from '../../../models/product.class';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-product-item',
   standalone: true,
-  imports: [MatButtonModule, MatIconModule],
+  imports: [MatButtonModule, MatIconModule, CommonModule],
   templateUrl: './product-item.component.html',
   styleUrl: './product-item.component.scss'
 })
 export class ProductItemComponent {
   @Input() product!: ProductClass;
+
+  description!: string;
+  longDescription: boolean = false;
+
+  constructor(){}
+
+  ngOnInit(): void {
+    this.getDescription(); 
+  }
+
+  getDescription(): void{
+    if(this.product.description.length > 100){
+      this.longDescription = true;
+      this.description = this.product.description.substring(0, 100);
+    }else{
+      this.longDescription = false;
+      this.description = this.product.description
+    }
+  }
 
 }
