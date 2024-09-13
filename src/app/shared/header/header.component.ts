3399b591096fb54acc566abebeec40053efc1391
Raudel Gómez Smith <gomezsmith.raudel@gmail.com>
2024-09-13 10:37:48 +0200

    page responsive/header fixing v1

--- src/app/shared/header/header.component.ts
+++ src/app/shared/header/header.component.ts
@@ -3,14 +3,40 @@ import {MatToolbarModule} from '@angular/material/toolbar';
 import {MatIconModule} from '@angular/material/icon';
 import {MatButtonModule} from '@angular/material/button';
 import { RouterModule } from '@angular/router';
+import {MatBadgeModule} from '@angular/material/badge';
+import { CartService } from '../../services/cart.service';
+import { map, Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [MatToolbarModule, MatIconModule, MatButtonModule, RouterModule],
+  imports: [MatToolbarModule, MatIconModule, MatButtonModule, RouterModule, MatBadgeModule],
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss'
 })
 export class HeaderComponent {
+  totalItems: number = 0;
+  subscription: Subscription = new Subscription();
 
+  constructor(private cartService: CartService){}
+
+  ngOnInit(): void {
+    this.cartService.getCartDataBase();
+    this.calcTotalItems();
+  }
+
+  ngOnDestroy(): void {
+    this.subscription.unsubscribe();    
+  }
+
+  calcTotalItems(){
+    const sub = this.cartService.getProductsCart()
+    .pipe(map(items =>{
+      return items.reduce((acc, curr)=> acc +  curr.productQuantity, 0)
+    }))
+    .subscribe(val =>{
+      this.totalItems = val;
+    });
+    this.subscription.add(sub);
+  }
 }
